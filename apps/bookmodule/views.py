9d05a073766718958ee2ca229585acab20516b9d
JoudSa <joud.i@outlook.com>
2026-03-01 23:41:40 +0300

    Upload full project

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -12,12 +12,24 @@ def viewbook(request, bookId):
     context = {'book':targetBook} # book is the variable name accessible by the template
     return render(request, 'bookmodule/show.html', context)
 
-def index(request):
+#def index(request):
    #return HttpResponse("Hello, world!")
    #return HttpResponse("Hello, "+name)
-   name = request.GET.get("name") or "world!"
-   return render(request, "bookmodule/index.html",{"name": name})    
+   #name = request.GET.get("name") or "world!"
+   #return render(request, "bookmodule/index.html",{"name": name})    
 
 def index2(request, val1 = 0):   #add the view function (index2)
     return HttpResponse("value1 = "+str(val1))
+ 
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
  
--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -57,6 +57,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [( os.path.join(BASE_DIR, "apps/static"))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
