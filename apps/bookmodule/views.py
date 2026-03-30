ab22a19a7b5770913a7911b91acae12cc81714e4
JoudSa <joud.i@outlook.com>
2026-03-30 11:34:37 +0300

    Upload full project

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -33,3 +33,39 @@ def viewbook(request, bookId):
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
  
+
+def links_view(request):
+    return render(request, 'bookmodule/links.html')
+
+def formatting_view(request):
+    return render(request, 'bookmodule/text_formatting.html')
+
+def listing_view(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables_view(request):
+    return render(request, 'bookmodule/tables.html')
+
+def searchView(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        # now filter
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodule/bookList.html', {'books':newBooks})
+    return render(request, 'bookmodule/search.html')
+
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
